aaa0ac2397277e4db7fcab147b03d2a60fe68746
Davi Silva <davisilvaphoto@gmail.com>
2025-11-15 09:25:05 -0300

    add signup usecase

---
+++ src/application/usecases/signup-usecase.ts
@@ -0,0 +1,32 @@
+import { AccountAlreadyExistsError } from '../errors/account-already-exists'
+import { prismaClient } from '../libs/prisma-client'
+import { hash } from 'bcryptjs'
+
+interface IInput {
+  name: string
+  email: string
+  password: string
+}
+
+type IOutput = void
+
+export class SignupUseCase {
+  async execute({ name, email, password }: IInput): Promise<IOutput> {
+    const accountAlreadyExists = await prismaClient.account.findUnique({
+      where: { email },
+    })
+
+    if (accountAlreadyExists)
+      throw new AccountAlreadyExistsError('Account already exists')
+
+    const hashedPassword = await hash(password, 10)
+
+    await prismaClient.account.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+  }
+}
